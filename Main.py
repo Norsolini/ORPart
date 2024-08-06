b3436c60e3c2a94aac360a4febe4ac236bb62c81
Norsolini <138036041+Norsolini@users.noreply.github.com>
2024-08-06 18:23:27 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid import AgGrid
+from st_aggrid import GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 
